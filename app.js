463b153b7462ae02dbdd499360ae4ba8c46aad75
MuhammadAl-Amin <alaminmillat24434@gmail.com>
2024-02-19 20:51:02 +0600

    javascript attachment

---
+++ app.js
@@ -0,0 +1,67 @@
+const allBtn = document.getElementsByClassName('add-btn');
+let availableSeat = 40;
+let seat = 0;
+
+for (const btn of allBtn){
+    btn.addEventListener('click', function(e) {
+        btn.style.backgroundColor = "#1DD100";
+        availableSeat = availableSeat - 1;
+        seat = seat + 1;
+        const seatNumber = e.target.innerText;
+
+        const mainContainer = document.getElementById('seat-details');
+        const li = document.createElement('li');
+        li.style.display = 'flex';
+        li.style.justifyContent = "space-around"
+        const p = document.createElement('p');
+        p.innerText = seatNumber;
+        const p1 = document.createElement('p');
+        p1.innerText = 'Economic';
+        const p2 = document.createElement('p');
+        p2.innerText = 550;
+
+        li.appendChild(p);
+        li.appendChild(p1);
+        li.appendChild(p2);
+        mainContainer.appendChild(li);
+
+        let costAmount = parseInt(document.getElementById('cost').innerText);
+        let totalCost = parseInt(p2.innerText);
+        
+        const sum = costAmount + totalCost;
+
+
+        addInnerText('cost', sum);
+        addInnerText('non-booked', availableSeat);
+        addInnerText('seat-badge', seat)
+        grandTotal('cost', sum)
+        
+    })
+    
+}
+
+
+function addInnerText(id, value){
+    document.getElementById(id).innerText = value;
+}
+
+function grandTotal(){
+    let grandTotalAmount = document.getElementById('cost').innerText;
+    
+
+    let couponValue = document.getElementById('coupon-value').value;
+    if(couponValue === 'NEW15'){
+        let newOffer = .15 * grandTotalAmount;
+        let newOfferTotal = grandTotalAmount - newOffer;
+        addInnerText('grand-total', newOfferTotal);
+    }
+    else if(couponValue === 'couple 20'){
+        let coupleOffer = .20 * grandTotalAmount;
+        let coupleOfferTotal = grandTotalAmount - coupleOffer;
+        addInnerText('grand-total', coupleOfferTotal);
+    }
+    else{
+        console.log('Please take a valid coupon.')
+        addInnerText('grand-total', grandTotalAmount);
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log('What is your father name.')
+let clickHandler = false;
+function colorAdding(elementId){
+    let buttonColor = document.getElementById(elementId);
+    buttonColor.classList.add('bg-[#1DD100]');
+    
+}
+
+function nonBookedSeats(elementId){
+    let nonBookedSeatCount = document.getElementById(elementId);
+    let nonBookedSeatCountNumber = nonBookedSeatCount.innerText;
+    let nonBookedSeatCountStringToNumber = parseInt(nonBookedSeatCountNumber);
+    let nonBookedSeatNumber = nonBookedSeatCountStringToNumber - 1;
+    nonBookedSeatCount.innerText = nonBookedSeatNumber;
+    return nonBookedSeatCountNumber;
+    
+}
+
+function seatDetails(elementId1, elementId2) {
+    let seatDetailsId = document.getElementById(elementId1);
+    let seatNumber = document.getElementById(elementId2).innerText;
+
+    let seatNumberAdded = document.createElement('p');
+    seatNumberAdded.textContent = seatNumber;
+    seatDetailsId.appendChild(seatNumberAdded);
+
+    let ticketClass = document.createElement('p');
+    ticketClass.textContent = 'Economic';
+    seatDetailsId.appendChild(ticketClass);
+
+    let ticketPrice = document.createElement('p');
+    ticketPrice.textContent = 550;
+    seatDetailsId.appendChild(ticketPrice);
+
+    
+}
+
+function setClick(){
+   if(!clickHandler){
+    colorAdding('a1');
+    nonBookedSeats('non-booked');
+    seatDetails('seat-details', 'a1')
+   }
+
+   clickHandler = true;
+}
+
+function setA2(){
+    if(!clickHandler){
+        colorAdding('a2');
+        nonBookedSeats('non-booked');
+        seatDetails('seat-details', 'a2')
+       }
+       clickHandler = true;
+}
